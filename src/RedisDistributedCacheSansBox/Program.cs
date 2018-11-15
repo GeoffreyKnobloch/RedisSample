83a816e1503d397846f1e75343f4877a11bce3c2
Geoffrey Knobloch <knobloch@doxense.com>
2018-11-15 17:52:11 +0100

    Commit samples

---
+++ src/RedisDistributedCacheSansBox/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RedisDistributedCacheSansBox
+{
+    using Microsoft.Extensions.Caching.Distributed;
+    using Microsoft.Extensions.DependencyInjection;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Redis distributed cache!");
+
+
+            var services = new ServiceCollection()
+                .AddDistributedRedisCache(options =>
+                {
+                    options.Configuration = "127.0.0.1";
+                    options.InstanceName = "Master";
+                }).BuildServiceProvider();
+            
+            // Redis distributed cache :
+            var distributedCache = services.GetService<IDistributedCache>();
+            string cacheKey = "myKey";
+            var myValue = await distributedCache.GetStringAsync(cacheKey);
+
+            Console.WriteLine($"First call to Redis distributed cache, received value: {myValue}");
+            
+            if (string.IsNullOrEmpty(myValue))
+            {
+                await distributedCache.SetStringAsync(cacheKey, "ValueToAdd");
+                var myValueAfterInsert = await distributedCache.GetStringAsync(cacheKey);
+                Console.WriteLine($"Second call to Redis distributed cache, received value: {myValueAfterInsert}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/RedisPubSubSandBox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace RedisPubSubSandBox
+{
+    using System;
+    using StackExchange.Redis;
+    class Program
+    {
+        private const string RedisConnectionString = "127.0.0.1:6379"; // Alternatively you can configure a connection to Redis with Azure.
+        private const string MyChannel = "Master";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Redis pub sub!");
+            
+            using (var connect = await ConnectionMultiplexer.ConnectAsync(RedisConnectionString))
+            {
+                var subscriber = connect.GetSubscriber();
+
+                // React on a message on channel :
+                await subscriber.SubscribeAsync(MyChannel, OnMessageReceived);
+
+                // Pub a message on channel 
+                await subscriber.PublishAsync(MyChannel, "My Message !");
+            }
+
+            Console.ReadKey();
+        }
+
+        // This method will be executed when a message is published on channel "Master" :
+        private static void OnMessageReceived(RedisChannel channel, RedisValue receivedValue)
+        {
+            Console.WriteLine($"Message received : {receivedValue}");
+        }
+    }
+}
